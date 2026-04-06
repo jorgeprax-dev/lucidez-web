191eaa2d1114e292298dbf6ee0117be5d673d482
Jorge Prax <jorge@sintelo.com>
2026-04-05 20:49:29 -0600

    feat: PostHog analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
+import posthog from 'posthog-js'
 import './index.css'
-import App from './App'
+import App from './App.jsx'
+
+posthog.init('phc_wryEhDEJRXwUaFCs9FiakzRcW4unmBbBU3oaBfTj3z6W', {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'identified_only',
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
